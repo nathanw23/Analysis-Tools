047c541b10e5822eb3047e58beb39f8bc30da90e
nathanw23 <94845836+nathanw23@users.noreply.github.com>
2022-10-06 16:09:31 +0100

    Update Video_Colour_Analyser.py

--- video_analysis_scripts/Video_Colour_Analyser.py
+++ video_analysis_scripts/Video_Colour_Analyser.py
@@ -12,6 +12,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import RectangleSelector
 import os
 from tqdm import tqdm
 import click
@@ -25,38 +26,16 @@ def TotalFrames(videofile):
     cap.release()
     print(f"The video has {FramesTotal} frames")
     
-    
-def ClipFirstFrame(videofile):
-    while True:
-        global Y1,Y2,X1,X2
-        vidcap = cv2.VideoCapture(videofile)
-        success, image = vidcap.read()
-        if success:
-            plt.figure()
-            plt.imshow(image)
-            plt.draw()
-            plt.pause(0.001)     
-        Y1 = int(input("Enter Y1 Coordinate: "))
-        Y2 = int(input("Enter Y2 Coordinate: "))
-        X1 = int(input("Enter X1 Coordinate: "))
-        X2 = int(input("Enter X2 Coordinate: "))    
-        plt.close()   
-        clip = image[Y1:Y2, X1:X2, :]
-        plt.figure()
-        plt.imshow(clip)
-        plt.draw()
-        plt.pause(0.001)      
-        Selection_Check = input("Are you happy with the selected area? (Y/N): ").capitalize()
-        if Selection_Check == "Y":
-            plt.close()
-            break
-        elif Selection_Check == "N":
-            plt.close()
-            print("Please select again!")
-        else:
-            plt.close()
-            print("Sorry, that is an invalid command!")
-
+def onselect_function(eclick, erelease):
+    global X1, X2, Y1, Y2
+    extent = rect_selector.extents
+    plt.xlim(extent[0], extent[1])
+    plt.ylim(extent[2], extent[3])
+    X1 = int(extent[0])
+    X2 = int(extent[1])
+    Y1 = int(extent[2])
+    Y2 = int(extent[0])
+    return X1, X2, Y1, Y2
         
 def process_video(video_filepath, Y1, Y2, X1, X2):
     global FramesTotal,colours,total_length
@@ -68,7 +47,7 @@ def process_video(video_filepath, Y1, Y2, X1, X2):
         grabbed, frame = cap.read()
         if not grabbed:
             break
-        current_frame = frame[Y1:Y2, X1:X2, :]
+        current_frame = frame[X1:X2, Y1:Y2, :]
         avg_color_per_row = np.average(current_frame, axis=0)
         avg_color = np.average(avg_color_per_row, axis=0).tolist()
         colours.append(avg_color)
@@ -84,13 +63,23 @@ def Analyse_Colour(video_file):
     exp_name = video_file.split(os.sep)[-1]
     exp_name = exp_name.rsplit('.', 1)[0]
     Video_FILE = video_file
-
-        
-    print("Counting Frames...")
     
     TotalFrames(Video_FILE)
-    
-    ClipFirstFrame(Video_FILE)
+
+    vidcap = cv2.VideoCapture(Video_FILE)
+    success, image = vidcap.read()
+    if success:
+        fig, current_ax = plt.subplots()  
+        plt.imshow(image)
+        
+    while True:
+        global rect_selector
+        rect_selector = RectangleSelector(current_ax, onselect_function, drawtype='box', button=[1])
+        plt.draw()
+        plt.pause(0.001)
+        input("Press Enter to continue...")
+        plt.close()
+        break
     
     process_video(Video_FILE, Y1, Y2, X1, X2)
     
