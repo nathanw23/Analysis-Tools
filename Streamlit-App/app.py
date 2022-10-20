d9e81f6fd8a93aac4d150ad4584714f0874f18b8
nathanw23 <94845836+nathanw23@users.noreply.github.com>
2022-10-20 09:00:24 +0100

    Added streamlit

---
+++ Streamlit-App/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Settings
+analysis_tools = ["Plate Multiplex"]
+page_title = "Analysis Tools Web App"
+page_icon = ":microscope:" # https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+ 
+# Navigation Menu
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title)
+
+# Upload data file and select analysis tool
+st.header("Select Data File & Select Tool")
+with st.form("data_form", clear_on_submit=True):
+    data_file = st.file_uploader("Select Data File", key="data_file")
+    st.selectbox("Select the analysis tool", analysis_tools, key="tool")
+    submitted = st.form_submit_button("Run Analysis")
+
+# Data Clean Up and Visualisation
+if submitted:
+    df = pd.read_csv(data_file)
+    #st.dataframe(df)
+    if st.session_state['tool'] == 'Plate Multiplex':
+        fig = plt.figure(figsize=(10,4))
+        sns.lineplot(data = df, x = "Converted_Time", y = "Signal", hue = "Group", style = "Fluorophore", ci = 'sd')
+        st.pyplot(fig)
