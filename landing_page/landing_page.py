816c2a20d0567087b9b544f29c9453898e18fe0b
nathanw23 <94845836+nathanw23@users.noreply.github.com>
2023-02-08 16:20:05 +0000

    Updated landing page

--- landing_page/landing_page.py
+++ landing_page/landing_page.py
@@ -1,28 +1,26 @@
-import pyfiglet
-from terminaltables import AsciiTable
 import click
+from rich.console import Console
+from rich import print
+from rich.table import Table
 
 
 @click.command()
 def landing_page():
-    Welcome = pyfiglet.figlet_format("Analysis Tools")
-    print(Welcome)
+    table = Table(title="Analysis Tools")
 
-    print("A collection of tools to help with data analysis and visualisation.")
+    table.add_column("Tool", style='Cyan', no_wrap=True)
+    table.add_column("Descripption", style="green")
 
-    table_data = [
-        ['Tool', 'Description'],
-        ['plate_kinetics', 'Plots kinetics data collected from the ClarioStar plate reader'],
-        ['plate_multiplex', 'Cleans up and plots multiplex kinetics data obtained from a ClarioStar plate reader'],
-        ['EDWIN_kinetics', 'Plots kinetics data collected from the BMG plate reader'],
-        ['fluor_scan', 'Plots scan data collected from a Cary Eclipse Fluorescence Spectrophotometer'],
-        ['fluor_kinetics', 'Plots kinetic scan data collected from a Cary Eclipse Fluorescence Spectrophotometer'],
-        ['fluor_3dscan', 'Plots 3D scan data collected from a Cary Eclipse Fluorescence Spectrophotometer'],
-        ['fluor_multiplex', 'Plots multiplex kinetics fluorescent data from a Cary Eclipse Fluorescence Spectrophotometer'],
-        ['plot_nanodrop', 'Plots specrea from a NanoDrop 2000c'],
-        ['plot_videocolour', 'Plots the RGB values for a specified region of a video'],
-        ['split_video', 'Split a video into individual frames'],
-    ]
+    table.add_row('plate_kinetics', 'Plots kinetics data collected from the ClarioStar plate reader')
+    table.add_row('plate_multiplex', 'Cleans up and plots multiplex kinetics data obtained from a ClarioStar plate reader')
+    table.add_row('EDWIN_kinetics', 'Plots kinetics data collected from the BMG plate reader')
+    table.add_row('fluor_scan', 'Plots scan data collected from a Cary Eclipse Fluorescence Spectrophotometer')
+    table.add_row('fluor_kinetics', 'Plots kinetic scan data collected from a Cary Eclipse Fluorescence Spectrophotometer')
+    table.add_row('fluor_3dscan', 'Plots 3D scan data collected from a Cary Eclipse Fluorescence Spectrophotometer')
+    table.add_row('fluor_multiplex', 'Plots multiplex kinetics fluorescent data from a Cary Eclipse Fluorescence Spectrophotometer')
+    table.add_row('plot_nanodrop', 'Plots specrea from a NanoDrop 2000c')
+    table.add_row('plot_videocolour', 'Plots the RGB values for a specified region of a video')
+    table.add_row('split_video', 'Split a video into individual frames')
 
-    table = AsciiTable(table_data)
-    print(table.table)
+    console = Console()
+    console.print(table)
